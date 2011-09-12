ad7b05ad381eb3a3b61ff207e291e1828d800638
özgür <hinoglu@gmail.com>
2011-09-12 11:45:28 +0300

    updated setup.py for new release

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 
+import os
 from distutils.core import setup
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(name='djtemps',
-      version='0.1',
-      description='Django - Jinja2 Templates Integration',
-      author='Ozgur Sefik Altunyurt',
-      author_email='altunyurt@gmail.com',
-      url='http://github.com/altunyurt/djtemps',
-      packages=['djtemps', 'djtemps/translation' ],
-     )
+    version='0.1.1',
+    description='Django - Jinja2 Templates Integration',
+    author='Ozgur Sefik Altunyurt',
+    author_email='altunyurt@gmail.com',
+    url='http://github.com/altunyurt/djtemps',
+    packages=['djtemps', 'djtemps/translation' ],
+    long_description=read('README'),
+    license = 'GPL',
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Text Processing :: Markup :: HTML"
+    ]
+)
